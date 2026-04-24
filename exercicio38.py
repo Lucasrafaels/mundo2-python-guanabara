5b63a264888bab303aaa397d4cdf1c820f5553fc
Lucas Rafael Silva <lucas23rafaels@gmail.com>
2026-04-24 16:31:55 -0300

    Adição de exercícios

---
+++ exercicio38.py
@@ -0,0 +1,8 @@
+primeiro_numero = int(input('Digite um número: '))
+segundo_numero = int(input('Digite um segundo número: '))
+if primeiro_numero > segundo_numero:
+    print(f'O número {primeiro_numero} é maior que {segundo_numero}')
+elif segundo_numero > primeiro_numero:
+    print(f'O número {segundo_numero} é maior que o {primeiro_numero}')
+else:
+    print ('Os dois números são iguais')
