2818000967f5745ca7adc3f46ed2ea8c5ef42997
Lucas Rafael Silva <lucas23rafaels@gmail.com>
2026-04-24 16:28:50 -0300

    Adição dos exercícios 38 e 39

--- exercicio37.py
+++ exercicio37.py
@@ -1,3 +1,5 @@
+#Conversor de bases númericas
+
 numero_escolhido = int(input('Digite um número para ser convertido para outra base númerica: '))
 print ('Digite [ 1 ] para Binário')
 print ('Digite [ 2 ] para Octal')
@@ -9,3 +11,5 @@ elif opcao_de_escolha == 2:
     print (f'O número {numero_escolhido} em binário é igual a:{oct(numero_escolhido)[2:]}')
 elif opcao_de_escolha == 3:
     print (f'O número {numero_escolhido} em binário é igual a:{hex(numero_escolhido)[2:]}')
+else:
+    print('Você não digitou nenhuma das opções')
