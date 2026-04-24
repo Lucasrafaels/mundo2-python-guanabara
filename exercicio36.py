08e570fa0f9ffcae3496357ce2ae5e51c8408668
Lucas Rafael Silva <lucas23rafaels@gmail.com>
2026-04-24 16:08:59 -0300

    questões 36 e 37

---
+++ exercicio36.py
@@ -0,0 +1,9 @@
+valor_casa = float(input('Digite o valor da casa que você que comprar: '))
+salario_comprador = float(input('Digite o seu salário: '))
+anos_de_pagamento = int(input('Digite em quantos anos pretende pagar:'))
+prestacao_mensal = valor_casa / (anos_de_pagamento * 12)
+limite = salario_comprador * (30/100)
+if limite >= prestacao_mensal:
+    print ('O empréstimo foi concedidio')
+else:
+    print ('O seu empréstimo não foi aprovado')
---
+++ exercicio37.py
@@ -0,0 +1,11 @@
+numero_escolhido = int(input('Digite um número para ser convertido para outra base númerica: '))
+print ('Digite [ 1 ] para Binário')
+print ('Digite [ 2 ] para Octal')
+print ('Digite [ 3 ] para Hexadecimal')
+opcao_de_escolha = int(input('Digite a Sua opão de escolha: '))
+if opcao_de_escolha == 1:
+    print (f'O número {numero_escolhido} em binário é igual a:{bin(numero_escolhido)[2:]}')
+elif opcao_de_escolha == 2:
+    print (f'O número {numero_escolhido} em binário é igual a:{oct(numero_escolhido)[2:]}')
+elif opcao_de_escolha == 3:
+    print (f'O número {numero_escolhido} em binário é igual a:{hex(numero_escolhido)[2:]}')
